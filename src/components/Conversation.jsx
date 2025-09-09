e3d59e9e15dd3542236c3bbb78b8f594ea6247e0
Saajan Bhujel <87616854+saajanbhujel@users.noreply.github.com>
2025-09-09 07:24:43 +0000

    installed ai-sdk and created a sample .env file

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -5,13 +5,13 @@ const Conversation = () => {
         <div className="p-2 h-120 text-base-content overflow-auto custom-scrollbar flex flex-col-reverse no-arrows">
             <div className="flex flex-col space-y-2">
                 <div className="flex justify-end">
-                    <div class="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
+                    <div className="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
                         <p>first Message</p>
                     </div>
                 </div>
 
                 <div className="flex justify-start">
-                    <div class="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
+                    <div className="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
                         <p>
                             Hi i love to help you. Hi i love to help you. Hi i
                             love to help you.
@@ -20,13 +20,13 @@ const Conversation = () => {
                 </div>
 
                 <div className="flex justify-end">
-                    <div class="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
+                    <div className="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
                         <p>second Message</p>
                     </div>
                 </div>
 
                 <div className="flex justify-start">
-                    <div class="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
+                    <div className="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
                         <p>
                             Hi i love to help you. Hi i love to help you. Hi i
                             love to help you.
@@ -35,13 +35,13 @@ const Conversation = () => {
                 </div>
 
                 <div className="flex justify-end">
-                    <div class="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
+                    <div className="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
                         <p>3rd Message</p>
                     </div>
                 </div>
 
                 <div className="flex justify-start">
-                    <div class="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
+                    <div className="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
                         <p>
                             Hi i love to help you. Hi i love to help you. Hi i
                             love to help you.
@@ -50,13 +50,13 @@ const Conversation = () => {
                 </div>
 
                 <div className="flex justify-end">
-                    <div class="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
+                    <div className="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
                         <p>4th Message</p>
                     </div>
                 </div>
 
                 <div className="flex justify-start">
-                    <div class="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
+                    <div className="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
                         <p>
                             Hi i love to help you. Hi i love to help you. Hi i
                             love to help you.
@@ -65,13 +65,13 @@ const Conversation = () => {
                 </div>
 
                 <div className="flex justify-end">
-                    <div class="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
+                    <div className="bg-base-100 rounded-2xl rounded-tr-sm p-2 max-w-[90%] border-2 border-blue-600 shadow-sm">
                         <p>latest Message</p>
                     </div>
                 </div>
 
                 <div className="flex justify-start">
-                    <div class="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
+                    <div className="bg-base-100 border-2 border-base-content/30 rounded-2xl rounded-tl-sm p-2 max-w-[90%] shadow-sm">
                         <p>
                             Hi i love to help you. Hi i love to help you. Hi i
                             love to help you.
